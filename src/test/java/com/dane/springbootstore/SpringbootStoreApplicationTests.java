13ac43c7ec47f0c3212c03186b9cb9224c2311f2
dane <keiko19970908@gmail.com>
2022-04-14 13:49:19 +0800

    SpringBoot store project 初始化

---
+++ src/test/java/com/dane/springbootstore/SpringbootStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dane.springbootstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
